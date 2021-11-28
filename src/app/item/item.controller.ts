f93b34efff3c05956e481179bc3aabe59addfc09
ruggero.panzeri95@gmail.com <ruggero.panzeri95@gmail.com>
2021-11-28 15:42:10 +0100

    missing await retrieving items

--- src/app/item/item.controller.ts
+++ src/app/item/item.controller.ts
@@ -2,9 +2,9 @@ import * as express from "express"
 export const router = express.Router()
 import {SuccessResponse} from "./../../core/apiResponse"
 import { ItemRepository } from "./item.repository"
-router.get('/', (req,res,next)=>{
+router.get('/', async (req,res,next)=>{
     try{
-        const items = ItemRepository.getAll()
+        const items = await ItemRepository.getAll()
         return new SuccessResponse('Items',items).send(res)
     }catch(e){
         next(e)

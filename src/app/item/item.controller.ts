8fd5a25517f121de11c95bf4d9bd1bf85bb97326
ruggero.panzeri95@gmail.com <ruggero.panzeri95@gmail.com>
2021-11-28 16:44:58 +0100

    added new api get single item and post an item

--- src/app/item/item.controller.ts
+++ src/app/item/item.controller.ts
@@ -1,6 +1,6 @@
 import * as express from "express"
 export const router = express.Router()
-import {SuccessResponse} from "./../../core/apiResponse"
+import {BadRequestResponse, SuccessResponse} from "./../../core/apiResponse"
 import { ItemRepository } from "./item.repository"
 router.get('/', async (req,res,next)=>{
     try{
@@ -10,3 +10,33 @@ router.get('/', async (req,res,next)=>{
         next(e)
     }
 })
+
+router.get('/:id', async (req,res,next)=>{
+    try{
+        const id = req.params.id
+        const item = await ItemRepository.getById(id)
+        return new SuccessResponse('Item',item).send(res)
+    }catch(e){
+        next(e)
+    }
+})
+
+
+router.post('/add', async (req,res,next)=>{
+    try{
+        
+        const name = req.body.name
+        const price = req.body.price
+        const weight = req.body.weight
+       
+        if(!name || !price || !weight){
+            return new BadRequestResponse().send(res)
+        }
+        const item = await ItemRepository.create(name, price, weight)
+        return new SuccessResponse('Item',{item_id:item[0]}).send(res)
+    }catch(e){
+        next(e)
+    }
+})
+
+
--- src/app/item/item.repository.ts
+++ src/app/item/item.repository.ts
@@ -1,8 +1,13 @@
 import { tables } from "./../../constants/sql_tables";
 import { connection as knex} from "./../../db/db";
+import dayjs from "dayjs"
 export class ItemRepository{
-    static create(){
+    static create(name:string|number, price:string|number,weight:string|number){
+        return knex(tables.TABLE_ITEMS).insert({name:name, price:price, weight:weight, created_at:dayjs().format('YYYY-MM-DD HH:mm:ss')})
+    }
 
+    static getById(item_id:number|string){
+        return knex.select().from(tables.TABLE_ITEMS).where({id:item_id})
     }
 
     static getAll(){

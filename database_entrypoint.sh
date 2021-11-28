d27135f4d34e951c1e5883dac1ab7c635a319b92
ruggero.panzeri95@gmail.com <ruggero.panzeri95@gmail.com>
2021-11-28 15:41:18 +0100

    script per aspettare database ed eseguire migration/seed/node

--- database_entrypoint.sh
+++ database_entrypoint.sh
@@ -1,4 +1,23 @@
-sleep 3
-npm run knex:migrate:latest
-npm run knex:seeds:latest
-node build/index
+#!/bin/sh
+retval="n"
+RESULT="Connection refused"
+
+check(){
+case "$RESULT" in
+  *refused*) retval="n" ;;
+  *)         retval="s" ;;
+esac
+}
+
+while  [ "$retval" != "s" ]
+do
+    echo 'waiting for database'
+    sleep 1
+   
+    RESULT=$(wget  http://db:3306 -nc 2>&1)
+    
+    check
+done
+  npm run knex:migrate:latest
+  npm run knex:seed:run
+  node build/index
